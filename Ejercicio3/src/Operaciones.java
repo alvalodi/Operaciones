7e2ef1fcf1dfb1603b78b4b5f83b1813e68d3e07
jsalor <jorgesalor24@gmail.com>
2016-05-06 10:53:57 +0200

    primer paso

---
+++ Ejercicio3/src/Operaciones.java
@@ -0,0 +1,11 @@
+
+
+	public class Operaciones {
+		public int multiplicacion(int n1, int n2){
+		int op;
+		op = n1 * n2;
+		
+		return op;
+		}
+		}
+
---
+++ Ejercicio3/src/OperacionesTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OperacionesTest {
+
+	@Test
+	public void test() {
+		
+	}
+	@before
+	
+
+}
